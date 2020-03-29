b7e516ff142760a8d386ddf8ebcb885f3bc17bcd
sharath bablu <bablu.sharath23@gmail.com>
2020-03-29 15:17:06 +0530

    fixed logger in store

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
 const loggerMiddleware = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
     let result = next(action)
+    console.log('next state', store.getState())
     console.groupEnd()
     return result
 }
